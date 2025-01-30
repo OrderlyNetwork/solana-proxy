e5ca5d24302f5f100ee81e7ef6a8b2d000816509
Zion <mrzz@tuta.io>
2025-01-31 00:23:27 +0100

    init proxy as oapp

--- programs/solana-proxy/src/state/proxy_config.rs
+++ programs/solana-proxy/src/state/proxy_config.rs
@@ -1,14 +1,29 @@
 use anchor_lang::prelude::*;
+use oapp::endpoint::{instructions::RegisterOAppParams, ID as ENDPOINT_ID};
 
 #[account]
 #[derive(InitSpace)]
 pub struct ProxyConfig {
     pub bump: u8,
-    pub owner: Pubkey,
-    pub nonce: u64,
-    pub dst_eid: u32,
+    pub endpoint_program: Pubkey,
+    pub usdc_token_account: Pubkey,
+    pub admin: Pubkey,
+    pub orderly_eid: u32,
     pub sol_chain_id: u128,
-    pub mint: Pubkey,
-    pub oft_program: Pubkey,
-    pub occ_manager_address: [u8; 32]
+    pub paused: bool,
 }
+
+#[account]
+#[derive(InitSpace)]
+pub struct LzReceiveTypesAccounts {
+    pub proxy_config: Pubkey,
+    // pub account_list: Pubkey, // point to the AccountList pda, should be updated if a new AccountList applied
+}
+
+// #[account]
+// #[derive(InitSpace)]
+// pub struct AccountList {
+//     pub bump: u8,
+//     pub usdc_pda: Pubkey,
+//     // can add more pda accounts here in the future with different seeds
+// }
