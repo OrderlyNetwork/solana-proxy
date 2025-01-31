53cec6094f522256f82dbc739c2d8491888197b4
Zion <mrzz@tuta.io>
2025-01-31 21:35:32 +0100

    set config for solana proxy

--- tasks/proxy/setProxy.ts
+++ tasks/proxy/setProxy.ts
@@ -2,14 +2,11 @@ import { task } from 'hardhat/config'
 import { Program, workspace, BN } from '@coral-xyz/anchor'
 import { types as devtoolsTypes } from '@layerzerolabs/devtools-evm-hardhat'
 import {
-    getProxyConfigPda,
-    getLzReceiveTypesPda,
-    getPeerPda,
     getPeerAddress,
     setupAnchor,
     printProxyConfig,
-    printPeerPda,
     getConfig,
+    printPeerPda,
     getDeployedProxyProgram,
     updateConfig,
     getEndpoint,
@@ -18,13 +15,25 @@ import {
     getSolanaChainId,
     getTokenATA,
     createAndSendV0Tx,
-    getSendLibProgramId,
-    getReceiveLibProgramId,
     getOptions,
     delay,
+    getUmi,
+    getEncodedOptions,
+    intoIx,
+    getLzConfig,
 } from './utils'
-import { SolanaProxy } from '../../target/types/solana_proxy'
-import { PublicKey, AccountMeta } from '@solana/web3.js'
+import {
+    getProxyConfigPda,
+    getLzReceiveTypesPda,
+    getPeerPda,
+    getOAppRegistryPda,
+    getReceiveLibConfigPda,
+    getDefaultReceiveLibConfigPda,
+    getSendLibProgramId,
+    getReceiveLibProgramId,
+    getDefaultSendLibConfigPda,
+} from './pdaHelper'
+import { PublicKey, AccountMeta, Connection } from '@solana/web3.js'
 import { isAddress } from 'web3-validator'
 import {
     fromWeb3JsInstruction,
@@ -43,12 +52,11 @@ import {
 import { Options } from '@layerzerolabs/lz-v2-utilities'
 import { oft } from '@layerzerolabs/oft-v2-solana-sdk'
 import { PEER_ADDRESS } from './constants'
+import { config } from 'process'
 
 task('sol:proxy:init', 'Create and init Proxy Config PDA')
     .addParam('env', 'The environment to run the task', undefined, devtoolsTypes.string)
     .setAction(async (taskArgs, hre) => {
-        const config = getConfig()
-
         const [provider, wallet] = setupAnchor(taskArgs.env)
         console.log('Wallet:', wallet.publicKey.toBase58())
         const proxyProgram = getDeployedProxyProgram(taskArgs.env, provider)
@@ -59,6 +67,11 @@ task('sol:proxy:init', 'Create and init Proxy Config PDA')
         const orderlyEid = getOrderlyEid(taskArgs.env)
         const solChainId = getSolanaChainId(taskArgs.env)
         const proxyTokenAccount = getTokenATA(usdcTokenAccount, proxyConfigPda)
+        const oappRegistryPda = getOAppRegistryPda(proxyConfigPda)
+        const admin = wallet
+        const peerAddress = getPeerAddress(taskArgs.env)
+
+        console.log('OApp Registry PDA:', oappRegistryPda.toBase58())
         try {
             const proxyConfig = await proxyProgram.account.proxyConfig.fetch(proxyConfigPda)
             printProxyConfig(proxyConfig)
@@ -92,6 +105,15 @@ task('sol:proxy:init', 'Create and init Proxy Config PDA')
         }
 
         try {
+            const initOAppNonceIx = endpoint.initOAppNonce(admin.publicKey, orderlyEid, proxyConfigPda, peerAddress!)
+            const tx = await createAndSendV0Tx([initOAppNonceIx], provider, wallet)
+            console.log('Tx to init OApp Nonce for Solana Proxy:', tx)
+            await delay(taskArgs.env)
+        } catch {
+            console.log('OApp Nonce already initialized')
+        }
+
+        try {
             const initSendLibIx = endpoint.initSendLibrary(wallet.publicKey, proxyConfigPda, orderlyEid)
             const tx = await createAndSendV0Tx([initSendLibIx], provider, wallet)
             console.log('Tx to init Send Lib for Solana Proxy:', tx)
@@ -116,7 +138,6 @@ task('sol:proxy:init', 'Create and init Proxy Config PDA')
             console.log('Tx to set Send Lib for Solana Proxy:', tx)
             await delay(taskArgs.env)
         } catch (e) {
-            console.log(e)
             console.log('Receive lib already set')
         }
 
@@ -137,7 +158,7 @@ task('sol:proxy:init', 'Create and init Proxy Config PDA')
         }
     })
 
-task('sol:proxy:set', 'Set Peer for Solana Proxy')
+task('sol:proxy:setpeer', 'Set Peer Config for Solana Proxy')
     .addParam('env', 'The environment to run the task', undefined, devtoolsTypes.string)
     .setAction(async (taskArgs, hre) => {
         const [provider, wallet] = setupAnchor(taskArgs.env)
@@ -155,6 +176,7 @@ task('sol:proxy:set', 'Set Peer for Solana Proxy')
         const admin = createNoopSigner(fromWeb3JsPublicKey(wallet.publicKey))
         const oftStore = fromWeb3JsPublicKey(proxyConfigPda)
         const options = getOptions(taskArgs.env)
+        const [optionSend, optionSendAndCall] = getEncodedOptions(options)
         const programId = fromWeb3JsPublicKey(proxyProgram.programId)
 
         const wrappedIx = [
@@ -174,13 +196,8 @@ task('sol:proxy:set', 'Set Peer for Solana Proxy')
                 { oftStore: oftStore, admin: admin },
                 {
                     __kind: 'EnforcedOptions',
-                    send: Options.newOptions()
-                        .addExecutorLzReceiveOption(options.LZ_RECEIVE_GAS, options.LZ_RECEIVE_VALUE)
-                        .toBytes(),
-                    sendAndCall: Options.newOptions()
-                        .addExecutorLzReceiveOption(options.LZ_RECEIVE_GAS, options.LZ_RECEIVE_VALUE)
-                        .addExecutorComposeOption(0, options.LZ_COMPOSE_GAS, options.LZ_COMPOSE_VALUE)
-                        .toBytes(),
+                    send: optionSend,
+                    sendAndCall: optionSendAndCall,
                     remote: orderlyEid,
                 },
                 programId
@@ -199,8 +216,219 @@ task('sol:proxy:set', 'Set Peer for Solana Proxy')
             ),
         ]
 
-        const ix = wrappedIx.map((wrapped) => toWeb3JsInstruction(wrapped.instruction))
+        const ix = intoIx(wrappedIx)
         const tx = await createAndSendV0Tx(ix, provider, wallet)
         console.log('Tx to set Peer for Solana Proxy:', tx)
         await delay(taskArgs.env)
+    })
+
+task('sol:proxy:setconfig', 'Set Config for Solana Proxy')
+    .addParam('env', 'The environment to run the task', undefined, devtoolsTypes.string)
+    .setAction(async (taskArgs, hre) => {
+        const [provider, wallet] = setupAnchor(taskArgs.env)
+        const proxyProgram = getDeployedProxyProgram(taskArgs.env, provider)
+        const proxyConfigPda = getProxyConfigPda(proxyProgram.programId)
+        const oftStore = fromWeb3JsPublicKey(proxyConfigPda)
+        const programId = fromWeb3JsPublicKey(proxyProgram.programId)
+        const orderlyEid = getOrderlyEid(taskArgs.env)
+        console.log(orderlyEid)
+        console.log(oftStore)
+        const rpc = getUmi(taskArgs.env).rpc
+        const admin = createNoopSigner(fromWeb3JsPublicKey(wallet.publicKey))
+
+        try {
+            const initIx = [
+                oft.initConfig(
+                    {
+                        admin: admin,
+                        oftStore: oftStore,
+                        payer: admin,
+                    },
+                    orderlyEid
+                ),
+            ]
+            const ix = intoIx(initIx)
+            const tx = await createAndSendV0Tx(ix, provider, wallet)
+            console.log('Tx to init Config for Solana Proxy:', tx)
+            await delay(taskArgs.env)
+        } catch (e) {
+            console.log('Config already initialized')
+        }
+
+        const connection = new Connection(rpc.getEndpoint(), 'confirmed')
+        const config = getLzConfig(orderlyEid)
+        const ix = [
+            await oft.setConfig(
+                connection,
+                {
+                    signer: admin.publicKey,
+                    oftStore: oftStore,
+                },
+                {
+                    remoteEid: orderlyEid,
+                    configType: 1, // EXECUTOR
+                    config: {
+                        maxMessageSize: config.sendLibConfig.executorConfig.maxMessageSize,
+                        executor: new PublicKey(config.sendLibConfig.executorConfig.executorAddress),
+                    },
+                }
+            ),
+            await oft.setConfig(
+                connection,
+                {
+                    signer: admin.publicKey,
+                    oftStore: oftStore,
+                },
+                {
+                    remoteEid: orderlyEid,
+                    configType: 2, // SEND ULN
+                    config: {
+                        confirmations: config.sendLibConfig.ulnConfig.confirmations,
+                        requiredDvnCount: config.sendLibConfig.ulnConfig.requiredDVNCount,
+                        optionalDvnCount: config.sendLibConfig.ulnConfig.optionalDVNCount,
+                        optionalDvnThreshold: config.sendLibConfig.ulnConfig.optionalDVNThreshold,
+                        requiredDvns: config.sendLibConfig?.ulnConfig.requiredDVNs.map(
+                            (address) => new PublicKey(address)
+                        ), // [new Web3PublicKey(config.sendLibConfig?.ulnConfig.requiredDVNs[0]!)]
+                        optionalDvns: [],
+                    },
+                }
+            ),
+
+            await oft.setConfig(
+                connection,
+                {
+                    signer: admin.publicKey,
+                    oftStore: oftStore,
+                },
+                {
+                    remoteEid: orderlyEid,
+                    configType: 3, // RECEIVE ULN
+                    config: {
+                        confirmations: config.receiveLibConfig?.ulnConfig.confirmations,
+                        requiredDvnCount: config.receiveLibConfig?.ulnConfig.requiredDVNCount,
+                        optionalDvnCount: config.receiveLibConfig?.ulnConfig.optionalDVNCount,
+                        optionalDvnThreshold: config.receiveLibConfig?.ulnConfig.optionalDVNThreshold,
+                        requiredDvns: config.receiveLibConfig?.ulnConfig.requiredDVNs.map(
+                            (address) => new PublicKey(address)
+                        ), // [new Web3PublicKey(config.sendLibConfig?.ulnConfig.requiredDVNs[0]!)]
+                        optionalDvns: [],
+                    },
+                }
+            ),
+        ]
+
+        const web3Ix = ix.map((ix) => toWeb3JsInstruction(ix))
+        const tx = await createAndSendV0Tx(web3Ix, provider, wallet)
+        console.log('Tx to set Config for Solana Proxy:', tx)
+    })
+
+task('sol:proxy:getconfig', 'Get Config for Solana Proxy')
+    .addParam('env', 'The environment to run the task', undefined, devtoolsTypes.string)
+    .setAction(async (taskArgs, hre) => {
+        const [provider] = setupAnchor(taskArgs.env)
+        const proxyProgram = getDeployedProxyProgram(taskArgs.env, provider)
+        const proxyConfigPda = getProxyConfigPda(proxyProgram.programId)
+        const oftStore = fromWeb3JsPublicKey(proxyConfigPda)
+        const programId = fromWeb3JsPublicKey(proxyProgram.programId)
+        const orderlyEid = getOrderlyEid(taskArgs.env)
+        const rpc = getUmi(taskArgs.env).rpc
+
+        const delegate = await oft.getDelegate(rpc, oftStore)
+        console.log('Delegate:', delegate.toString())
+        const peerAddress = await oft.getPeerAddress(rpc, oftStore, orderlyEid, programId)
+        console.log('Peer Address:', peerAddress)
+
+        const enforcedOptions = await oft.getEnforcedOptions(rpc, oftStore, orderlyEid, programId)
+        const options = getOptions(taskArgs.env)
+        const [optionSend, optionSendAndCall] = getEncodedOptions(options)
+        console.log(
+            `The option config for send: receive gas = ${options.LZ_RECEIVE_GAS}, receive value = ${options.LZ_RECEIVE_VALUE}`
+        )
+        console.log(`The option config into bytes:    `, Buffer.from(optionSend).toString('hex'))
+        console.log(`The option set onchain for send: `, Buffer.from(enforcedOptions.send).toString('hex'))
+        console.log(
+            `The option config for sendAndCall: receive gas = ${options.LZ_RECEIVE_GAS}, receive value = ${options.LZ_RECEIVE_VALUE}, compose gas = ${options.LZ_COMPOSE_GAS}, compose value = ${options.LZ_COMPOSE_VALUE}`
+        )
+        console.log(`The option config into bytes:           `, Buffer.from(optionSendAndCall).toString('hex'))
+        console.log(
+            `The option set onchain for sendAndCall: `,
+            Buffer.from(enforcedOptions.sendAndCall).toString('hex')
+        )
+
+        try {
+            const endpointConfig = await oft.getEndpointConfig(rpc, oftStore, orderlyEid)
+            // sleep for 2
+            console.log(`Endpoint config - send lib config: `) // , endpointConfig.sendLibraryConfig
+            console.log(`    - messageLib: `, endpointConfig.sendLibraryConfig.messageLib.toString())
+            console.log(`    - uln sendConfig: `)
+            console.log(`        - uln: `)
+            console.log(
+                `           - confirmation: `,
+                endpointConfig.sendLibraryConfig.ulnSendConfig?.uln.confirmations.toString()
+            )
+            console.log(
+                `           - requiredDvns:: `,
+                endpointConfig.sendLibraryConfig.ulnSendConfig?.uln.requiredDvns.toString()
+            )
+            console.log(
+                `           - requiredDvnCount: `,
+                endpointConfig.sendLibraryConfig.ulnSendConfig?.uln.requiredDvnCount
+            )
+            console.log(
+                `           - optionalDvns: `,
+                endpointConfig.sendLibraryConfig.ulnSendConfig?.uln.optionalDvns.toLocaleString()
+            )
+            console.log(
+                `           - optionalDvnCount: `,
+                endpointConfig.sendLibraryConfig.ulnSendConfig?.uln.optionalDvnCount
+            )
+            console.log(
+                `           - optionalDvnThreshold: `,
+                endpointConfig.sendLibraryConfig.ulnSendConfig?.uln.optionalDvnThreshold
+            )
+            console.log(`        - executor: `)
+            console.log(
+                `           - maxMessageSize: `,
+                endpointConfig.sendLibraryConfig.ulnSendConfig?.executor.maxMessageSize
+            )
+            console.log(
+                `           - executor: `,
+                endpointConfig.sendLibraryConfig.ulnSendConfig?.executor.executor.toString()
+            )
+
+            console.log(`Endpoint config - receive lib config: `) // , endpointConfig.receiveLibraryConfig
+            console.log(`    - messageLib: `, endpointConfig.receiveLibraryConfig.messageLib.toString())
+            console.log(`    - timeout: `, endpointConfig.receiveLibraryConfig.timeout)
+            console.log(`    - uln receiveConfig: `)
+            console.log(`        - uln: `)
+            console.log(
+                `           - confirmation: `,
+                endpointConfig.receiveLibraryConfig.ulnReceiveConfig?.uln.confirmations.toString()
+            )
+            console.log(
+                `           - requiredDvns:: `,
+                endpointConfig.receiveLibraryConfig.ulnReceiveConfig?.uln.requiredDvns.toString()
+            )
+            console.log(
+                `           - requiredDvnCount: `,
+                endpointConfig.receiveLibraryConfig.ulnReceiveConfig?.uln.requiredDvnCount
+            )
+            console.log(
+                `           - optionalDvns: `,
+                endpointConfig.receiveLibraryConfig.ulnReceiveConfig?.uln.optionalDvns.toLocaleString()
+            )
+            console.log(
+                `           - optionalDvnCount: `,
+                endpointConfig.receiveLibraryConfig.ulnReceiveConfig?.uln.optionalDvnCount
+            )
+            console.log(
+                `           - optionalDvnThreshold: `,
+                endpointConfig.receiveLibraryConfig.ulnReceiveConfig?.uln.optionalDvnThreshold
+            )
+        } catch (e) {
+            console.log(e)
+            // console.log("Using default endpoint config")
+            console.log(`🛎️ Config for orderly network not set yet, please set it`)
+        }
     })
