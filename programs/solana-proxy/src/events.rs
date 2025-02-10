5b75f5be308e37bcf4d9ba159e135bda95043d88
Zion <mrzz@tuta.io>
2025-02-10 20:18:09 +0100

    udpate events

--- programs/solana-proxy/src/events.rs
+++ programs/solana-proxy/src/events.rs
@@ -23,3 +23,14 @@ pub struct ClaimRequestSent {
     pub cumulative_amount: [u8; 32],
     pub merkle_root: [u8; 32],
 }
+
+#[event]
+pub struct AdminRoleTransferred {
+    pub old_admin: Pubkey,
+    pub new_admin: Pubkey,
+}
+
+#[event]
+pub struct ProxyPaused {
+    pub paused: bool,
+}
