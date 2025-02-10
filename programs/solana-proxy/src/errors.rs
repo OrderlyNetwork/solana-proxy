dae207dbec8aeb91aeefa063f6014045b60aa1e0
Zion <mrzz@tuta.io>
2025-02-10 13:58:59 +0100

    udpate errors

--- programs/solana-proxy/src/errors.rs
+++ programs/solana-proxy/src/errors.rs
@@ -3,6 +3,26 @@ use anchor_lang::prelude::error_code;
 // Vault errors
 #[error_code]
 pub enum ProxyError {
-    #[msg("Proxy owner is not the same as the payer")]
-    InvalidProxyOwner,
+    #[msg("Signer is not the Proxy admin")]
+    InvalidProxyAdmin,
+    #[msg("Rate limit exceeded")]
+    RateLimitExceeded,
+    #[msg("Invalid fee")]
+    InvalidFee,
+    #[msg("Invalid mint authority")]
+    InvalidMintAuthority,
+    #[msg("Solana Proxy is Paused")]
+    Paused,
+    #[msg("User is not the owner of the account")]
+    InvalidUser,
+    #[msg("Invalid payload type")]
+    InvalidPayloadType,
+    #[msg("Invalid USDC account")]
+    InvalidUSDCAccount,
+    #[msg("Invalid receiver")]
+    InvalidReceiver,
+    #[msg("Invalid Ledger payload type")]
+    InvalidLedgerPayloadType,
+    #[msg("Invalid sender")]
+    InvalidSender,
 }
