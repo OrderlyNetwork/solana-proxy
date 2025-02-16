21a7d34630a52123d11ed86ad9eab2430b53de22
Zion <mrzz@tuta.io>
2025-02-16 22:11:09 +0100

    add ledger oapp into repo

---
+++ contracts/lib/ILedgerOCCManager.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
+
+import { OCCVaultMessage, EvmLedgerMessage } from "./OCCTypes.sol";
+
+interface ILedgerOCCManager {
+    function ledgerSendToVault(EvmLedgerMessage memory message) external payable;
+
+    function collectUnvestedOrders(uint256 amount) external;
+
+    function ledgerOappReceive(OCCVaultMessage calldata message) external;
+}
